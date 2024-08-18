dc61d414c106952717e7acc55cf73b8127366d6e
ting4324 <114212854+ting4324@users.noreply.github.com>
2024-08-18 22:03:51 +0800

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << end1;
+    cout << "What is your age?" ;
+    <in >> age;
+
+    cout << "My age is?" << age << end1;
+
+    return 0;
+}
